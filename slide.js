2b8ba7827c3aa24258ffc830278374a4aee5f956
azenkin3 <93234779+azenkin3@users.noreply.github.com>
2021-12-15 16:14:59 +0900

    1215

---
+++ slide.js
@@ -0,0 +1,36 @@
+var sliderWrapper = document.querySelector('.slider_wrapper'),
+sliderUl = sliderWrapper.querySelector('.slides'),
+slides = sliderUl.querySelectorAll('li'),
+currentIdx = 0,
+slideCount = slides.length,
+slideWidth = 300,
+slideMargin = 30,
+prevBtn = document.querySelector('#prev'),
+nextBtn = document.querySelector('#next');
+
+sliderUl.style.width = (slideWidth*slideCount) + slideMargin*(slideCount-1) + 'px';
+
+
+function moveSlide(idx){
+sliderUl.style.left = -idx * (slideWidth + slideMargin)+'px';
+currentIdx = idx;
+}
+
+nextBtn.addEventListener('click',function(){
+if(currentIdx == slideCount - 3){
+    moveSlide(0);
+}else{
+    moveSlide(currentIdx + 1);
+}
+});
+prevBtn.addEventListener('click',function(){
+if(currentIdx == 0){
+    moveSlide(slideCount - 3);
+}else{
+    moveSlide(currentIdx - 1);
+}
+});
+
+
+/* queryseletor , moveslide idx 유튜브보고 찾아보기 
+*/
