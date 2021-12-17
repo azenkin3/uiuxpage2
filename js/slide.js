83774e2aeaf53442ca5f0a0ce29d1fdfdfa83cc4
azenkin3 <93234779+azenkin3@users.noreply.github.com>
2021-12-17 15:46:48 +0900

    123

--- js/slide.js
+++ js/slide.js
@@ -1,5 +1,3 @@
-
-
         var sliderWrapper = document.querySelector('.slider_wrapper'),
 sliderUl = sliderWrapper.querySelector('.slides'),
 slides = sliderUl.querySelectorAll('li'),
@@ -32,3 +30,33 @@ moveSlide(slideCount - 3);
 moveSlide(currentIdx - 1);
 }
 });
+
+/* counter */
+
+
+$('.counter').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+        
+        $({ countNum: $this.text()}).animate({
+          countNum: countTo
+        },
+      
+        {
+      
+          duration: 8000,
+          easing:'linear',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+           
+          }
+      
+        });  
+        
+        
+      
+      });
+      
