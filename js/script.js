5beb63b02a916a026825b2266327016524f0d4f8
azenkin3 <93234779+azenkin3@users.noreply.github.com>
2021-12-19 14:59:31 +0900

    12345

---
+++ js/script.js
@@ -0,0 +1,127 @@
+
+
+$(window).scroll(function () {
+    var ws = $(this).scrollTop();
+    if (matchMedia("screen and (min-width:800px)").matches) {
+
+    if (ws > 450) {
+        $('article').css("transform", "translateY(0px)").css("opacity", "1")      
+    }
+
+
+    if (ws > 850) {
+        $('.carda').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 850) {
+        $('.cardb').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 850) {
+        $('.cardc').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 850) {
+        $('.cardd').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    
+    if (ws > 850) {
+        $('.oli').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 850) {
+        $('.pli').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
+    /* -- iconbox ~ popular listing -  */
+
+
+
+    /* -- wrappage2 ~ lets find out -  */
+    if (ws > 1450) {
+        $('.photob').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 1450) {
+        $('#fo').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 1530) {
+        $('#go').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 1600) {
+        $('#pfo1').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 1650) {
+        $('#pfo2').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 1800) {
+        $('#ch1').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 1800) {
+        $('#ch2').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 1800) {
+        $('#ch3').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 1800) {
+        $('#ch4').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 1800) {
+        $('.counter').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    /* -- wrappage2 ~ lets find out -  */
+
+
+    /* -- wrappage2 ~ lets find out -  */
+   
+    if (ws > 2300) {
+        $('#videobox').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 3100) {
+        $('#hli').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
+    if (ws > 3150) {
+        $('#aeh').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 3800) {
+        $('#reviewbox').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 3950) {
+        $('#ttl').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
+    if (ws > 3980) {
+        $('#hpe').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
+    if (ws > 4100) {
+        $('.slider_wrapper').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
+
+    }
+
+});
--- js/slide.js
+++ js/slide.js
@@ -49,7 +49,7 @@ $('.counter').each(function() {
       
         {
       
-          duration: 8000,
+          duration: 30000,
           easing:'linear',
           step: function() {
             $this.text(Math.floor(this.countNum));
