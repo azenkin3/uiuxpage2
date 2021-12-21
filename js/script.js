6482ace4a27521b68d60df323c1f123d5f56fb92
azenkin3 <93234779+azenkin3@users.noreply.github.com>
2021-12-21 17:46:32 +0900

    123

--- js/script.js
+++ js/script.js
@@ -138,7 +138,150 @@ $(window).scroll(function () {
         $('.slider_wrapper').css("transform", "translateX(0px)").css("opacity", "1")
     }
 
+}else{
+
+
+
+
+    if (ws > 100) {
+        $('article').css("transform", "translateY(0px)").css("opacity", "1")      
+    }
+
+
+    if (ws > 700) {
+        $('.carda').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 850) {
+        $('.cardb').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 850) {
+        $('.cardc').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 850) {
+        $('.cardd').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    
+    if (ws > 850) {
+        $('.oli').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 850) {
+        $('.pli').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
+    /* -- iconbox ~ popular listing -  */
+
+
+
+    /* -- wrappage2 ~ lets find out -  */
+    if (ws > 1450) {
+        $('.photob').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 1450) {
+        $('#fo').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 1530) {
+        $('#go').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 1600) {
+        $('#pfo1').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 1650) {
+        $('#pfo2').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 1800) {
+        $('#ch1').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 1800) {
+        $('#ch2').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 1800) {
+        $('#ch3').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 1800) {
+        $('#ch4').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+    if (ws > 1800) {
+        $('.counter').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    /* -- wrappage2 ~ lets find out -  */
+
+
+    /* -- wrappage2 ~ lets find out -  */
+   
+    if (ws > 2300) {
+        $('#videobox').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 3200) {
+        $('#hli').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
+    if (ws > 3250) {
+        $('#aeh').css("transform", "translateX(0px)").css("opacity", "1")
+
+        if (ws > 3300) {
+            $('.carde').css("transform", "translateY(0px)").css("opacity", "1")
+        }
+    
+        if (ws > 3300) {
+            $('.cardf').css("transform", "translateY(0px)").css("opacity", "1")
+        }
+    
+    
+        if (ws > 3300) {
+            $('.cardg').css("transform", "translateY(0px)").css("opacity", "1")
+        }
+    
+        if (ws > 3300) {
+            $('.cardh').css("transform", "translateY(0px)").css("opacity", "1")
+        }
+    }
+
+
+    if (ws > 3900) {
+        $('#reviewbox').css("transform", "translateY(0px)").css("opacity", "1")
+    }
+
+
+    if (ws > 4050) {
+        $('#ttl').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
+    if (ws > 4111) {
+        $('#hpe').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
+    if (ws > 4200) {
+        $('.slider_wrapper').css("transform", "translateX(0px)").css("opacity", "1")
+    }
+
 
     }
+
+
+
+
 
 });
--- js/slide.js
+++ js/slide.js
@@ -65,3 +65,24 @@ $('.counter').each(function() {
       
       });
       
+
+
+      
+$(function(){
+  $('.navbar__toggleBtn').click(function(){
+    $('.navbar__menu').slideToggle();
+  });
+  });
+  
+  $(window).resize(function(){
+    if(window.innerWidth>700){
+      $('.navbar__menu').show();
+      $('.navbar__toogleBtn').hide();
+    }
+  });
+  $(window).resize(function(){
+    if(window.innerWidth<700){
+      $('.navbar__menu').hide();
+  }  
+   
+  });
