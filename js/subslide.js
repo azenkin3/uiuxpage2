e5999a690f6a7f7529565129dcd09f2ee8cf8580
azenkin3 <93234779+azenkin3@users.noreply.github.com>
2021-12-29 14:47:20 +0900

    12345

---
+++ js/subslide.js
@@ -0,0 +1,16 @@
+
+
+window.onload = function(){
+
+function slide(){
+
+$('.slide li:last').prependTo('.slide');
+
+$('.slide').css({marginTop:-500});
+
+$('.slide').animate({marginTop:0});
+
+}
+
+setInterval(slide, 3000);
+}
