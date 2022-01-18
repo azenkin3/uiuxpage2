5ad05e53dbebbfc161425f6a9c20ce2dc6deae89
azenkin3 <93234779+azenkin3@users.noreply.github.com>
2022-01-18 17:39:34 +0900

    12345

--- js/count.js
+++ js/count.js
@@ -74,74 +74,7 @@
         });
 
         
-        $('#plus').click(function(evt) {
-            $('.line').each(function(idx, ele) {
-                $(ele).val(parseInt($(ele).val())+3043200);
-            });
-            evt.preventDefault();
-        });
-
-
-        $('#minus').click(function(evt) {
-            $('.line').each(function(idx, ele) {
-                $(ele).val(parseInt($(ele).val())-3043200);
-
-                if($("#amounts").val() < 0) {
-                    alert("최소 1명 이상 이어야 합니다.");
-                    $("#amounts").val(0); 
-                    $(".line").val(0);
-                  
-                }
-
-                   
-                
-            });
-            evt.preventDefault();
-        });
-
-
-        $('#plus2').click(function(evt) {
-            $('.line').each(function(idx, ele) {
-                $(ele).val(parseInt($(ele).val())+2749200);
-            });
-            evt.preventDefault();
-        });
-        $('#minus2').click(function(evt) {
-            $('.line').each(function(idx, ele) {
-                $(ele).val(parseInt($(ele).val())-2749200);
-                if($("#amountss").val() < 0) {
-                    alert("최소 1명 이상 이어야 합니다.");
-                    $("#amountss").val(0); 
-                    $(".line").val(0);
-                }
-                
-            });
-            evt.preventDefault();
-        });
-
-        $('#plus3').click(function(evt) {
-            $('.line').each(function(idx, ele) {
-                $(ele).val(parseInt($(ele).val())+558600);
-            });
-            evt.preventDefault();
-        });
-
-
-        $('#minus3').click(function(evt) {
-            $('.line').each(function(idx, ele) {
-                $(ele).val(parseInt($(ele).val())-558600);
-
-            
-
-                if($("#amountsss").val() < 0) {
-                    alert("최소 1명 이상 이어야 합니다.");
-                    $("#amountsss").val(0); 
-                    $(".line").val(0);
-                }
-                
-            });
-            evt.preventDefault();
-        });
+       
     
       
  
